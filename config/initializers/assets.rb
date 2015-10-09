ec8e7706758b70af1654c57bfb55b8ce3683f87b
stephan <railsc0d0r@gmail.com>
2015-10-09 22:39:49 +0200

    Adds modernizer.js to asset-pipeline. Refs #49

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,8 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w[ modernizr.js ]
+
+Rails.application.config.assets.precompile << Proc.new do |path|
+  path =~ /\.(css|js|png|jpg|mp3|ogg|wav|otf|eot|ttf|woff|fnt|svg|appcache|json)\z/ ? true : false
+end
