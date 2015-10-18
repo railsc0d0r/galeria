82ed1071034e3e34194f6b6bb8f9fcd7246f0dd8
stephan <railsc0d0r@gmail.com>
2015-10-18 22:20:02 +0200

    Generates user-model, migration, routes and stuff for authentication in backend. Refs #38, #44

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  # scope the backend to provide deeplinks for ember-app
+  scope '/backend' do
+    devise_for :users
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
