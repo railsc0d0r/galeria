9098be3315dabb91a7e923a84005e110b2c5194e
stephan <railsc0d0r@gmail.com>
2015-10-28 15:24:03 +0100

    Provides csrf-token over JSON-API-call. Resolves 'Processing by SessionsController#create as JSON - Can't verify CSRF token authenticity - 401 Unauthorized' issues. Refs #38, #44

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # scope the backend to provide deeplinks for ember-app
   scope '/backend' do
+    get :csrf, to: 'csrf#index'
     devise_for :users, controllers: { sessions: 'sessions' }
   end
 
