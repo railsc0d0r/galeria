5f7862c2359066207a48573ae5858ef0ef72ba2f
stephan <railsc0d0r@gmail.com>
2015-10-30 06:05:45 +0100

    Seeds users. Refs #38, #44

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Defining default users
+puts "Seeding users:\n\r"
+
+users = []
+
+marion = {
+  username: 'marion',
+  email: 'marion@marionrommel.de',
+  password: 'myArt_2015'
+}
+
+users << marion
+
+stephan = {
+  username: 'stephan',
+  email: 'railsc0d0r@gmail.com',
+  password: 'letMeIn_2015'
+}
+
+users << stephan
+
+users.each do |user|
+
+  user[:password_confirmation] = user[:password]
+
+  user[:activated] = true
+  user[:is_admin] = true
+  
+  old_user = User.find_by(username: user[:username], email: user[:email])
+
+  if old_user.nil?
+    puts "Creating user #{user[:username]} ->"
+    User.create!(user)
+    puts "Done."
+  else
+    puts "User #{user[:username]} already exists. Nothing to do here."
+  end
+end
