232263b0c9c5880bc6b48dbc82ac40e6fba1973b
stephan <railsc0d0r@gmail.com>
2015-10-30 23:59:49 +0100

    Transitions to application-root on successfull login. Refs #38, #44

--- frontend/app/controllers/login.js
+++ frontend/app/controllers/login.js
@@ -8,11 +8,16 @@ export default Ember.Controller.extend({
       let login = this.get('login');
       let password = this.get('password');
 
-      this.get('session').authenticate('authenticator:devise', login, password).catch((reason) => {
+      let session = this.get('session')
+      session.authenticate('authenticator:devise', login, password).catch((reason) => {
         this.set('errorMessage', reason.error);
         this.set('login', '');
         this.set('password', '');
       });
+      if (session.isAuthenticated) {
+        $.modal.close();
+        this.transitionToRoute('application');        
+      }
     },
     close() {
       $.modal.close();
