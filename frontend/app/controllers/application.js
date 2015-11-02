05cf17ac714bbc4fd3cea211cda024a37265eb97
stephan <railsc0d0r@gmail.com>
2015-11-02 21:26:15 +0100

    Removes useless stuff and makes it snappier.

--- frontend/app/controllers/application.js
+++ frontend/app/controllers/application.js
@@ -1,13 +1,16 @@
 import Ember from 'ember';
-import SessionService from 'ember-simple-auth/services/session';
 
 export default Ember.Controller.extend({
   session: Ember.inject.service('session'),
 
   actions: {
     invalidateSession() {
-      setTimeout(this._showPopUp("Logging out."),6000);
-      this.get('session').invalidate();
+      this._showPopUp("Logging out.");
+
+      var _this = this;
+      setTimeout(function(){
+        _this.get('session').invalidate();
+      },2300);
     },
     showSuccessfulAuthenticated() {
       this._showPopUp("Successfully authenticated.");
@@ -15,9 +18,8 @@ export default Ember.Controller.extend({
   },
 
   _showPopUp(text) {
-    console.log('Adding message popup');
     var div = "<div id='messagePopUp'>" + text + "</div>";
     $('body').prepend(div);
-    $('#messagePopUp').fadeOut(5000,function() {$('#messagePopUp').remove();});
+    $('#messagePopUp').fadeOut(2000,function() {$('#messagePopUp').remove();});
   }
 });
--- frontend/app/controllers/login.js
+++ frontend/app/controllers/login.js
@@ -1,5 +1,4 @@
 import Ember from 'ember';
-import SessionService from 'ember-simple-auth/services/session';
 
 export default Ember.Controller.extend({
   needs: ['application'],
