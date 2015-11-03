5b8dc932229bab92089ffa2a5028010120a6bf58
stephan <railsc0d0r@gmail.com>
2015-11-03 03:04:28 +0100

    Scaffolds picture in backend. Migrates db accordingly. Refs #30, #31

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151101145312) do
+ActiveRecord::Schema.define(version: 20151103015757) do
+
+  create_table "pictures", force: :cascade do |t|
+    t.string   "name"
+    t.boolean  "public",     default: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username",               default: "",    null: false
