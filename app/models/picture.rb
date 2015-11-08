a8324b1caaab3f61f9c8ecd42f0642ac587faa74
stephan <railsc0d0r@gmail.com>
2015-11-08 05:23:46 +0100

    Defines the path of an attachement to be RailsUpload.folder. Refs #30, #31

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,6 +1,10 @@
 class Picture < ActiveRecord::Base
 
-  has_attached_file :attachement, styles: { large: "1400x1050>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :attachement,
+                    styles: { large: "1400x1050>", thumb: "100x100>" },
+                    default_url: "/images/:style/missing.png",
+                    path: RailsUpload.folder
+  
   validates_attachment_content_type :attachement, content_type: /\Aimage\/.*\Z/
   validates_attachment_presence :attachement
   
