b5a4ebb86b4d4f67ee532b1fbf56d7da7330a1b8
stephan <railsc0d0r@gmail.com>
2015-11-13 00:15:09 +0100

    Defines path for paperclip in initializers/assets.rb now. Refs #30, #31

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -2,8 +2,7 @@ class Picture < ActiveRecord::Base
 
   has_attached_file :attachement,
                     styles: { large: "1400x1050>", thumb: "100x100>" },
-                    default_url: "/images/:style/missing.png",
-                    path: RailsUpload.folder
+                    default_url: "/images/:style/missing.png"
   
   validates_attachment_content_type :attachement, content_type: /\Aimage\/.*\Z/
   validates_attachment_presence :attachement
