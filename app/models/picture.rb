5ff0882fded727d0756c9e672f13675ce5f3c0f6
stephan <railsc0d0r@gmail.com>
2015-11-14 05:59:04 +0100

    Adds preprocessing to attachement. Refs #30, #31

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,8 +1,12 @@
 class Picture < ActiveRecord::Base
 
   has_attached_file :attachement,
-                    styles: { large: "1400x1050>", thumb: "100x100>" },
-                    default_url: "/images/:style/missing.png"
+                    processors: [:watermark],
+                    styles: { large: { geometry:"800x600>",
+                                       :watermark_path => Rails.root.join('watermark.png'),
+                                       :position => 'NorthWest'
+                                     },
+                              thumb: "100x100>" }
   
   validates_attachment_content_type :attachement, content_type: /\Aimage\/.*\Z/
   validates_attachment_presence :attachement
