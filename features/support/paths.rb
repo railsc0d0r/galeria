460d211f3cd3ab1e7e4a54745f6796edf8c5a4fc
stephan <railsc0d0r@gmail.com>
2015-11-15 21:18:48 +0100

    Adds paths for testing.

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -1,8 +1,12 @@
 # Method to map keywords to different paths of our app
 def path_for pathname
   case pathname
+  when 'Startseite'
+    "/"
   when 'Login'
     "/login"
+  when 'Bilderliste'
+    "/pictures"
   else
     raise "No path defined for page named '#{pagename}'."
   end
