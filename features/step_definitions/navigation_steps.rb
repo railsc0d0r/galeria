9269d8568140181f41267070d4f754794369b8c9
stephan <railsc0d0r@gmail.com>
2015-11-15 21:26:06 +0100

    Adds step-definition checking if i'm on the picture-list.

--- features/step_definitions/navigation_steps.rb
+++ features/step_definitions/navigation_steps.rb
@@ -6,3 +6,7 @@ end
 Wenn(/^ich zur Anmeldeseite gehe\.$/) do
   steps %{ Wenn ich zur "Login"-Seite gehe. }
 end
+
+Dann(/^möchte ich die Bilder\-Liste sehen\.$/) do
+  raise "Not on 'Bilderliste' but on #{page.current_path}'" unless page.current_path == path_for("Bilderliste")
+end
