bf3964dbbc7e037b8433fbd9254c276e3f4b1ce3
stephan <railsc0d0r@gmail.com>
2015-11-22 19:50:11 +0100

    Sets app up to send auth-headers in request when authenticated. Backend has to be updated accordingly. Refs #34

--- frontend/app/controllers/login.js
+++ frontend/app/controllers/login.js
@@ -2,15 +2,14 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   needs: ['application'],
-  session: Ember.inject.service('session'),
+  authManager: Ember.inject.service('session'),
 
   actions: {
     authenticate() {
       let login = this.get('login');
       let password = this.get('password');
 
-      let session = this.get('session');
-      session.authenticate('authenticator:devise', login, password).catch((reason) => {
+      this.get('authManager').authenticate('authenticator:devise', login, password).catch((reason) => {
         this.set('errorMessage', reason.error);
         this.set('login', '');
         this.set('password', '');
